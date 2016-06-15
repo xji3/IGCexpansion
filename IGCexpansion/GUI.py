a54b1b9a298561af3d932039cca8845e0e2bd26d
xji3 <xji3@ncsu.edu>
2016-06-15 19:11:31 -0400

    still working on GUI

--- IGCexpansion/GUI.py
+++ IGCexpansion/GUI.py
@@ -1,18 +1,58 @@
 # Trying to create a GUI for my IGCexpansion package
 # Xiang ji
 # xji3@ncsu.edu
-from traits.etsconfig.api import ETSConfig
+from traits.etsconfig.api import *
 ETSConfig.toolkit = 'qt4' #'wx'
-from traits.api import HasTraits, Str, Int
-from traitsui.api import View, Item
-import traitsui
+from traitsui.api import *
+from traits.api import *
+
+class TextDisplay(HasTraits):
+    string = String()
+
+    view = View( Item('string', show_label = False, springy = True, style = 'custom'))
 
 # This should be the Model part in MVC structure
 class SimpleTest(HasTraits):
-    Alignment = Str
+    Alignment_file = File
+    Alignment = Instance(TextDisplay)
+    _updated = Bool(False)
 
-view_test = View(Item(name = 'Alignment'))
+   # Display specification (one Item per editor style):
+    file_group = Group(
+        Item( 'file_name', style = 'simple',   label = 'Simple' ),
+        Item( '_' ),
+        Item( 'file_name', style = 'custom',   label = 'Custom' ),
+        Item( '_' ),
+        Item( 'file_name', style = 'text',     label = 'Text' ),
+        Item( '_' ),
+        Item( 'file_name', style = 'readonly', label = 'ReadOnly' )
+    )
+
+    def _Alignment_file_changed(self):
+        self.Alignment = '  '
+        
+    
+class TC_Handler(Handler):
+
+    def setattr(self, info, object, name, value):
+        Handler.setattr(self, info, object, name, value)
+        info.object._updated = True
+
+    def object__updated_changed(self, info):
+        if info.initialized:
+            info.ui.title += "*"
+
+
+
+class MainWindow(HasTraits):
+    display = Instance(TextDisplay, ())
+
+view_test = View(Item(name = 'Alignment_file'),
+                 title = 'Alter Title',
+                 handler = TC_Handler(),
+                 #buttons = [OKButton, CancelButton, Align])
+                 buttons = ['OK', 'Cancel'])
 
 if __name__ == '__main__':
     test = SimpleTest()
-    test.configure_traits()#view = view_test)
+    test.configure_traits(view = view_test)
