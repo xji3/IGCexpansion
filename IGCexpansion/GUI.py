ae95efc1e8fcc22d33092213f1ab481388c211c6
xji3 <xji3@ncsu.edu>
2016-06-17 16:55:20 -0400

    progress on GUI

--- IGCexpansion/GUI.py
+++ IGCexpansion/GUI.py
@@ -5,6 +5,8 @@ from traits.etsconfig.api import *
 ETSConfig.toolkit = 'qt4' #'wx'
 from traitsui.api import *
 from traits.api import *
+import os
+#from mpl_figure_editor import MPLFigureEditor
 
 class TextDisplay(HasTraits):
     string = String()
@@ -13,28 +15,48 @@ class TextDisplay(HasTraits):
 
 # This should be the Model part in MVC structure
 class Alignment(HasTraits):
-    Alignment_file = File
+    alignment_file = File
     display = Instance(TextDisplay)
     #Align   = Button()
     _updated = Bool(False)
 
-   # Display specification (one Item per editor style):
-    file_group = Group(
-        Item( 'file_name', style = 'simple',   label = 'Simple' ),
-        Item( '_' ),
-        Item( 'file_name', style = 'custom',   label = 'Custom' ),
-        Item( '_' ),
-        Item( 'file_name', style = 'text',     label = 'Text' ),
-        Item( '_' ),
-        Item( 'file_name', style = 'readonly', label = 'ReadOnly' )
-    )
+    view = View( Item('alignment_file', show_label = False))#, Item('Align', show_label = False))
 
-    view = View( Item('Alignment_file', show_label = False))#, Item('Align', show_label = False))
+    def _alignment_file_changed(self):
+        if os.path.isfile(self.alignment_file):
+            self.display.string = 'Found alignment file. \n' + self.display.string
+        else:
+            self.display.string = 'Alignment file does not exist. \n' + self.display.string
 
-    def _Alignment_file_changed(self):
-        self.Alignment = '  '
-        
-    
+# model enumeration list
+class SupportedModels(HasTraits):
+    model = Enum('MG94', 'HKY',)
+    force = Bool
+    clock = Bool
+
+    display = Instance(TextDisplay)
+
+    view = View( Group(Item(name = 'model',
+                            label = 'Implemented Models'),
+                       Item(name = 'force',
+                            style = 'simple',
+                            label = 'without tau parameter'),
+                       Item(name = 'clock',
+                            style = 'simple',
+                            label = 'strict molecular clock'),
+                       style = 'simple')
+                 )
+
+    def _model_changes(self):
+        self.display.string = 'Model set to ' + self.model + '\n' + self.display.string
+
+    def _force_changes(self):
+        self.display.string = 'Constrain tau to be 0. \n' + self.display.string
+
+    def _clock_changes(self):
+        self.display.string = 'Assume strict molecular clock. \n' + self.display.string
+
+
 class TC_Handler(Handler):
 
     def setattr(self, info, object, name, value):
@@ -45,26 +67,65 @@ class TC_Handler(Handler):
         if info.initialized:
             info.ui.title += "*"
 
+class Tree(HasTraits):
+    tree_file = File
+    display = Instance(TextDisplay)
+    view = View(Item('tree_file', show_label = True))
 
+    def _tree_file_changed(self):
+        if os.path.isfile(self.tree_file):
+            self.display.string = 'Found tree file. \n' + self.display.string
+        else:
+            self.display.string = 'Alignment file does not exist. \n' + self.display.string
+
+class Paralog(HasTraits):
+    paralog1 = Str
+    paralog2 = Str
+    view = View(Item('paralog1', show_label = False),
+                Item('paralog2', show_label = False))
 
 class MainWindow(HasTraits):
-    display = Instance(TextDisplay, ())
+    display = Instance(TextDisplay)
 
     alignment = Instance(Alignment)
 
+    model = Instance(SupportedModels)
+
+    tree = Instance(Tree)
+
+    paralog = Instance(Paralog)
+
     def _alignment_default(self):
         return Alignment(display = self.display)
-    view = View('display', 'alignment',
+
+##    def _paralog_changed(self):
+##        if not (self.paralog.paralog1 in self.alignment.alignment_file and
+##                self.paralog.paralog2 in self.alignment.alignment_file):
+##            self.display.string = 'Please make sure the alignment file is for this paralog. \n' + self.display.string
+##                
+
+    view = View(HSplit(Item('display', dock = 'vertical'),
+                       Group(Item('alignment', style = 'custom'),
+                             Item('_'),
+                             Item('model', style = 'custom'),
+                             Item('_'),
+                             Item('tree', style = 'custom'),
+                             Item('_'),
+                             Item('paralog', style = 'custom')
+                             ),
+                       show_labels = True),
+                height = 0.15, width = 0.25,
                 buttons = [OKButton, CancelButton],
                 title = 'IGC GUI',
                 style = 'custom', resizable=True)
 
-view_test = View(Item(name = 'Alignment_file'),
-                 title = 'Alter Title',
-                 handler = TC_Handler(),
-                 #buttons = [OKButton, CancelButton, Align])
-                 buttons = ['OK', 'Cancel'])
 
 if __name__ == '__main__':
-    test = MainWindow()
+##    container = Container(camera=Camera(), display=TextDisplay())
+##    container.configure_traits()
+    test = SupportedModels()
+##    test.configure_traits()
+
+    test = MainWindow(model = SupportedModels(), alignment = Alignment(), display = TextDisplay(), tree = Tree(),
+                      paralog = Paralog())
     test.configure_traits()
