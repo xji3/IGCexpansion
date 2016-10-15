c08b43ae339d26f00f7d0488545b60d99d02adad
xji3 <xji3@ncsu.edu>
2016-10-15 18:51:57 -0400

    add gap in data format

---
+++ test/Run.py
@@ -0,0 +1,19 @@
+from IGCexpansion.CodonGeneconv import ReCodonGeneconv
+
+if __name__ == '__main__':
+    paralog = ['YLR406C', 'YDL075W']
+    Force = None
+    alignment_file = './YLR406C_YDL075W_input.fasta'
+    newicktree = './YeastTree.newick'
+#     if args.force:
+#         if args.model == 'MG94':
+#             Force = {5:0.0}
+#         elif args.model == 'HKY':
+#             Force = {4:0.0}
+#     else:
+#         Force = None
+    Force = None
+    test = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = Force, clock = None)
+#     test.get_mle(True, True, 0, 'BFGS')
+    test.get_individual_summary(summary_path = './Summary/')
+    test.get_SitewisePosteriorSummary(summary_path = './Summary/')
