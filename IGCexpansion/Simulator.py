c61d289004ee7343134b565b4d9fb4b61535a735
xji3 <xji3@ncsu.edu>
2017-03-22 11:36:33 -0400

    forgot to push

--- IGCexpansion/Simulator.py
+++ IGCexpansion/Simulator.py
@@ -35,6 +35,9 @@ class Simulator:
         self.root_by_dup = self.tree.is_duplication_node(self.tree.phylo_tree.root.name)
         self.PMModel = PMModel(pm_model_name, x_pm, rate_variation)
         self.IGCModel = IGCTractModel(x_IGC, range(self.tree.n_orlg), pm_IGC)
+        self.gene_to_orlg_file = gene_to_orlg_file          # input from data class which is not used here
+        self.gene_to_orlg   = dict()         # dictionary used to store gene ortholog group info
+        self.orlg_to_gene   = dict()         # dictionary used to store gene ortholog group info
         self.seq_index_file = seq_index_file # seq_index file location
         self.seq_index = None                # store sequence index information
         self.nsites    = None                # number of nucleotide to simulate, must agree with the overall span seq_index
@@ -58,8 +61,23 @@ class Simulator:
         return 'IGC simulator output seq: ' + self.seq_file + '\n' + \
                'log file: ' + self.log_file + '\n'
 
+    def get_gene_to_orlg(self):  # copied from data class
+        assert(os.path.isfile(self.gene_to_orlg_file))
+        with open(self.gene_to_orlg_file, 'rb') as f:
+            for line in f:
+                items = line.split()
+                if items:
+                    gene = items[0]
+                    orlg = int(items[1])
+                    self.gene_to_orlg[gene] = orlg
+                    if orlg in self.orlg_to_gene:
+                        self.orlg_to_gene[orlg].append(gene.split('__'))
+                    else:
+                        self.orlg_to_gene[orlg] = [gene.split('__')]
+
     def initiate(self):
         self.get_seed_file()
+        self.get_gene_to_orlg()
         self.unpack_x_rates(self.x_rates)
         self.read_seq_index_file()
 
@@ -121,12 +139,14 @@ class Simulator:
                 seq = draw_from_distribution(distn, self.nsites, 'ACGT')
                 self.node_to_seq[root_name][orlg] = seq
 
-    def sim(self):
+    def sim(self, display = False):
         self.sim_root()
         # simulate sequences on each node by the path to each node in terminal_node_list
+        for terminal_node_name in self.terminal_node_list:
+            self.simulate_for_path(terminal_node_name, display)
 
 
-    def simulate_for_path(self, terminal_node_name):
+    def simulate_for_path(self, terminal_node_name, display):
         terminal_clade = self.tree.find_clade(terminal_node_name)
         path = self.tree.phylo_tree.get_path(terminal_clade)
         for clade in path:
@@ -135,19 +155,23 @@ class Simulator:
             else:
                 father_clade = self.tree.find_parent_clade(clade.name)
                 if father_clade.name in self.node_to_seq:
-                    edge = (father_clade.name, terminal_node_name)
-                    blen = self.tree.edge_to_blen[edge]
-                    print
+                    edge = (father_clade.name, clade.name)
+                    self.sim_one_branch(edge, display)
                 else:
                     print 'The node cannot be recognised!'
                     assert(False)
             
-    def sim_one_branch(self, starting_seq, blen, conf):
+    def sim_one_branch(self, edge, display):
+        # First, make sure this branch is not simulated
+        assert(edge[0] in self.node_to_seq and not edge[1] in self.node_to_seq)
+        blen = self.tree.edge_to_blen[edge]
+        starting_seq = self.node_to_seq[edge[0]]
+        conf = self.tree.node_to_conf[edge[0]]
         branch_orlg = divide_configuration(conf)
         
-        current_seq = deepcopy(starting_seq)
+        current_seq = deepcopy(starting_seq)  # it's passed on to the next node, need a new allocation of memory
         branch_orlg = divide_configuration(conf)
-        assert(all([orlg in branch_orlg['loc'] for orlg in starting_seq.keys()]))
+        #assert(all([orlg in branch_orlg['loc'] for orlg in starting_seq.keys()]))
 
         # Get sub IGC init matrix from
         ordered_orlg = sorted(branch_orlg['loc'])
@@ -155,12 +179,23 @@ class Simulator:
             Total_IGC_init_rate = 0.0
         else:
             branch_IGC_init_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
+            branch_IGC_tract_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
+            Total_IGC_init_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
             for i in range(len(ordered_orlg)):
                 for j in range(len(ordered_orlg)):
                     branch_IGC_init_Q[i, j] = self.IGCModel.Q_init[ordered_orlg[i], ordered_orlg[j]]
-            
+                    branch_IGC_tract_Q[i, j] = self.IGCModel.Q_tract[ordered_orlg[i], ordered_orlg[j]]
+                    if i != j:
+                        if branch_IGC_tract_Q[i, j] != 0:
+                            Total_IGC_init_Q[i, j] = branch_IGC_init_Q[i, j] * (self.nsites - 1 + 1.0 / branch_IGC_tract_Q[i, j])
+                    
             IGC_init_rate_diag = branch_IGC_init_Q.sum(axis = 1) # row sum
-            Total_IGC_init_rate = sum(IGC_init_rate_diag) * (self.nsites - 1 + 1/2)
+            Total_IGC_init_rate = Total_IGC_init_Q.sum()
+
+            sub_IGC_tract_Q = branch_IGC_tract_Q
+            sub_total_IGC_init_Q = Total_IGC_init_Q
+            sub_IGC_init_Q = branch_IGC_init_Q
+
 
         cummulate_time = 0.0
 
@@ -174,7 +209,9 @@ class Simulator:
             Total_rate = Total_PM_rate + Total_IGC_init_rate
 
             cummulate_time += np.random.exponential(1.0 / Total_rate)
-            print cummulate_time
+            if display:
+                print cummulate_time
+           
 
             if cummulate_time > blen :
                 break
@@ -185,11 +222,46 @@ class Simulator:
 
                 if event == 0:
                     # It's a point mutation event
-                    self.get_one_point_mutation(current_seq, seq_rate_dict)
+                    current_seq, mutation_info = self.get_one_point_mutation(current_seq, seq_rate_dict, display)
                 elif event == 1:
                     # It's an IGC event
-                    print
+                    current_seq, IGC_info = self.get_one_IGC_event(current_seq, branch_IGC_init_Q, branch_IGC_tract_Q, Total_IGC_init_Q, ordered_orlg, display)
+                else:
+                    # draw from distribution failure
+                    assert(False)
 
+        # Now need to pass the seq to new node
+        # need to consider gene duplication loss events here
+        self.pass_seq_to_node(current_seq, edge)
+
+    def pass_seq_to_node(self, seq, edge):
+        father_node = edge[0]
+        child_node = edge[1]
+        assert(self.tree.find_parent_clade(child_node).name == father_node) # make sure it's as expected
+
+        father_orlg = divide_configuration(self.tree.node_to_conf[father_node])
+        child_orlg = divide_configuration(self.tree.node_to_conf[child_node])
+
+        if set(father_orlg['loc']) == set(child_orlg['loc']):
+            self.node_to_seq[child_node] = seq
+        else:
+            father_changed_orlg = set(father_orlg['loc']) - set(child_orlg['loc'])
+            child_changed_orlg = set(child_orlg['loc']) - set(father_orlg['loc'])
+            assert(len(father_changed_orlg) == 1) # only allow one gene to duplicate or lose for now, for tandem duplication, should allow multiple
+            if len(child_changed_orlg) == 2: # duplication event
+                give_birth_orlg = list(father_changed_orlg)[0]
+                assert(set(self.tree.dup_events[give_birth_orlg]) == set(child_changed_orlg))
+                for new_orlg in child_changed_orlg:
+                    seq[new_orlg] = deepcopy(seq[give_birth_orlg])
+                seq.pop(give_birth_orlg)
+            elif len(child_changed_orlg) == 0: # a gene loss event
+                lost_orlg = list(father_changed_orlg)[0]
+                seq.pop(lost_orlg)
+            else:
+                assert(False) # should not come to this case
+
+            self.node_to_seq[child_node] = seq
+ 
     def get_mutation_rate(self, seq): # modified from IGCSimulation.IGCSimulator
         #print self.current_seq
         # TODO: add in rv
@@ -199,13 +271,18 @@ class Simulator:
         seq_rate_dict = dict()
         for orlg in seq.keys():
             seq_rate = [PM_diag_rates['ACGT'.index(seq[orlg][i])] for i in range(self.nsites)]
+            # My lazy compromise
+            if self.PMModel.rate_variation:
+                for i in range(len(seq_rate) / 3):
+                    seq_rate[3 * i + 1] = seq_rate[3 * i + 1] * self.PMModel.parameters['r2']
+                    seq_rate[3 * i + 2] = seq_rate[3 * i + 2] * self.PMModel.parameters['r3']
             seq_rate_dict[orlg] = seq_rate
             poisson_rate_sum += sum(seq_rate)
 
         return seq_rate_dict, poisson_rate_sum
 
 
-    def get_one_point_mutation(self, seq, seq_rate_dict): # modified from IGCSimulation.IGCSimulator
+    def get_one_point_mutation(self, seq, seq_rate_dict, display): # modified from IGCSimulation.IGCSimulator
         # only allow all sequences to be of same length
         assert(len(set([len(seq_rate_dict[orlg]) for orlg in seq_rate_dict])) == 1)
         orlg_group = sorted(seq_rate_dict.keys())
@@ -231,11 +308,12 @@ class Simulator:
         # TODO: implement log
         # mutation_orlg, mut_pos, old_state, new_state
         mutation_info = [str(mut_paralog), str(seq_pos), old_state, new_state]
-        print ' '.join(mutation_info)
+        if display:
+            print ' '.join(mutation_info)
 
         return seq, mutation_info
 
-    def get_one_IGC_event(self, seq, sub_IGC_init_Q, sub_IGC_tract_Q, sub_total_IGC_init_Q, ordered_orlg):
+    def get_one_IGC_event(self, seq, sub_IGC_init_Q, sub_IGC_tract_Q, sub_total_IGC_init_Q, ordered_orlg, display):
         np.fill_diagonal(sub_IGC_tract_Q, 0.0)  # fill diagonal entries with 0, just in case..
         
         # sample an IGC event orlg pair        
@@ -250,14 +328,17 @@ class Simulator:
         tract_p = sub_IGC_tract_Q[orlg_from_num, orlg_to_num]
         init_prob_array = np.array([1.0 / tract_p] + [1.0] * (self.nsites - 1))
         start_pos = draw_from_distribution(init_prob_array / sum(init_prob_array), 1, range(len(init_prob_array)))
-        tract_length = np.random.geometric(tract_p, 1)
+        tract_length = np.random.geometric(tract_p, 1)[0]
         stop_pos = start_pos + tract_length + 1
-        seq, IGC_info = self.IGC_copy(start_pos, stop_pos, orlg_from, orlg_to, seq)
+        if stop_pos > self.nsites:
+            stop_pos = self.nsites
+        #print start_pos, stop_pos, self.nsites
+        seq, IGC_info = self.IGC_copy(start_pos, stop_pos, orlg_from, orlg_to, seq, display)
 
         return seq, IGC_info
 
 
-    def IGC_copy(self, start_pos, stop_pos, orlg_from, orlg_to, seq):
+    def IGC_copy(self, start_pos, stop_pos, orlg_from, orlg_to, seq, display):
         # make sure the two orlgs are in seq
         assert(orlg_from in seq and orlg_to in seq)
         template_seq = seq[orlg_from][start_pos:stop_pos]
@@ -267,15 +348,29 @@ class Simulator:
 
         # TODO: implement log
         IGC_info = [str(orlg_from), str(orlg_to), str(start_pos), str(stop_pos), str(num_diff)]
-        print ' '.join(IGC_info)
+        if display:
+            print ' '.join(IGC_info)
 
-        # Now perform the IGC
-        print ''.join(template_seq), ''.join(overide_seq)
+        # Now perform the IGC event
+            print ''.join(template_seq), ''.join(overide_seq)
         for i in range(start_pos, stop_pos):
             seq[orlg_to][i] = seq[orlg_from][i]
 
-        print ''.join(template_seq), ''.join(overide_seq)
         return seq, IGC_info
+
+    def output_seq(self):
+        # output sequence into seq_file using fasta format
+        # use self.seq_index information to select sites being written into the file
+        with open(self.seq_file, 'w+') as f:
+            for name in self.terminal_node_list:
+                for orlg in self.node_to_seq[name]:
+                    gene_name_list = [i for i in self.orlg_to_gene[orlg] if i[0] == name ]
+                    assert(len(gene_name_list) == 1)
+                    f.write('>' + '__'.join(gene_name_list[0]) + '\n')
+                    f.write(''.join(self.node_to_seq[name][orlg]) + '\n')
+                    
+            
+
         
         
 
@@ -299,8 +394,8 @@ if __name__ == '__main__':
     #nsites = 489
 
     pm_model_name = 'HKY'
-    x_pm = np.log([0.4, 0.5, 0.2, 9.2])
-    rate_variation = False
+    x_pm = np.log([0.4, 0.5, 0.2, 9.2, 0.4, 5.0])
+    rate_variation = True
 
     x_IGC = [2.0, 0.3]
     init_pm = 'One rate'
@@ -326,78 +421,20 @@ if __name__ == '__main__':
 
     self = test
     #print test
-    test.sim()
+    display = False
+    test.sim(display = display)
+    test.output_seq()
 
-    edge = ('N0', 'D1')
-    edge = ('N0', 'kluyveri')
-    blen = self.tree.edge_to_blen[edge]
-    conf = self.tree.node_to_conf['N0']
-    #conf = self.tree.node_to_conf['N2']
-    starting_seq = self.node_to_seq['N0']
-
-    current_seq = deepcopy(starting_seq)
-    branch_orlg = divide_configuration(conf)
-    #assert(all([orlg in branch_orlg['loc'] for orlg in starting_seq.keys()]))
-
-    # Get sub IGC init matrix from
-    ordered_orlg = sorted(branch_orlg['loc'])
-    if len(ordered_orlg) == 1:
-        Total_IGC_init_rate = 0.0
-    else:
-        branch_IGC_init_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
-        branch_IGC_tract_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
-        Total_IGC_init_Q = np.zeros((len(ordered_orlg), len(ordered_orlg)), dtype = np.floating)
-        for i in range(len(ordered_orlg)):
-            for j in range(len(ordered_orlg)):
-                branch_IGC_init_Q[i, j] = self.IGCModel.Q_init[ordered_orlg[i], ordered_orlg[j]]
-                branch_IGC_tract_Q[i, j] = self.IGCModel.Q_tract[ordered_orlg[i], ordered_orlg[j]]
-                if i != j:
-                    if branch_IGC_tract_Q[i, j] != 0:
-                        Total_IGC_init_Q[i, j] = branch_IGC_init_Q[i, j] * (self.nsites - 1 + 1.0 / branch_IGC_tract_Q[i, j])
-                
-        IGC_init_rate_diag = branch_IGC_init_Q.sum(axis = 1) # row sum
-        Total_IGC_init_rate = Total_IGC_init_Q.sum()
-
-        sub_IGC_tract_Q = branch_IGC_tract_Q
-        sub_total_IGC_init_Q = Total_IGC_init_Q
-        sub_IGC_init_Q = branch_IGC_init_Q
-
-
-    cummulate_time = 0.0
-
-    while(cummulate_time < blen):
-        # Now sample exponential distributed waiting time for next event
-        # point mutation or IGC event
-        # need to update total point mutation rate with every new event
-        # no need to update total IGC rate on the same branch since it's modeled as context independent
-
-        seq_rate_dict, Total_PM_rate = self.get_mutation_rate( current_seq )
-        Total_rate = Total_PM_rate + Total_IGC_init_rate
-
-        cummulate_time += np.random.exponential(1.0 / Total_rate)
-        print cummulate_time
-
-        
-
-        if cummulate_time > blen :
-            break
-        else:
-            # Now decide whether it's a point mutation or IGC event
-            event = draw_from_distribution(np.array([Total_PM_rate, Total_IGC_init_rate]) / Total_rate,
-                                           1, range(2))
-
-            if event == 0:
-                # It's a point mutation event
-                current_seq, mutation_info = self.get_one_point_mutation(current_seq, seq_rate_dict)
-            elif event == 1:
-                # It's an IGC event
-                current_seq, IGC_info = self.get_one_IGC_event(current_seq, branch_IGC_init_Q, branch_IGC_tract_Q, Total_IGC_init_Q, ordered_orlg)
-        
+##    print "Still simulating?"
+##    edge = ('N0', 'D1')
+##    test.sim_one_branch(edge, True)
+##    
+##    edge = ('D1', 'N1')
+##    test.sim_one_branch(edge, True)
 
     
-    
 
-    
+
     
     
     #print test.node_to_seq
