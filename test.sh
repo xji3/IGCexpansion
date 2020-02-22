4d304efcfb571c692be7fc73c8500170bf4036d2
xji3 <xji3@ncsu.edu>
2020-02-22 10:46:23 -0800

    add more tests

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 cd IGCexpansion
 python Simulator.py
+python CodonGeneconv.py
+python Data.py
+python IGCModel.py
+python IGCTractModel.py
+python IndCodonGeneconv.py
+python JSGeneconv.py
+python PMModdel
+python PSIGCModel.py
+python PSJSGeneconv.py
+python PSJSModel.py
+python Tree.py
 
