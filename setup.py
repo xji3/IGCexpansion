7019c7e267c23b0dadd6b728f42a48593288a133
xji3 <xji3@ncsu.edu>
2020-02-22 15:02:49 -0800

    update setup for dependencies

--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ setup(name = 'IGCexpansion',
       url = 'https://github.com/xji3/IGCexpansion',
       #download_url = 'https://github.com/xji3/Genconv/tree/master/IGCexpansion/',
       packages = ['IGCexpansion',],
+      install_requires=[
+          'Biopython', 'networkx', 'numpy', 'scipy', #'jsonctmctree'
+      ],
+      dependency_links=[
+      'https://github.com/xji3/jsonctmctree/tarball/master#egg=package-0.2.0',
+      'git+https://github.com/xji3/jsonctmctree.git@master#egg=jsonctmctree-0.2.0'
+      ]
       #long_description = open('README.md').read()
       )
